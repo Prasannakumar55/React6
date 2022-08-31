a79c015bc002f6029577afe47a612c1e9580b4f3
Prasannakumar55 <prasannakumar55.pk7@gmail.com>
2022-09-01 03:40:15 +0530

    prasannakumar

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { BrowserRouter, Routes,Route} from 'react-router-dom';
+import { Home } from './Pages/Home';
+import { Students } from './Pages/Student';
+import { Contact } from './Pages/Contact';
+import { StudentContextProvider } from './StudentContext';
+
+function App() {
+  return (
+    <StudentContextProvider>
+    <BrowserRouter>
+    <Routes>
+     <Route path={'/'} element={<Home/>} />
+      <Route path={'/Pages/Student'} element={<Students/>} />
+      <Route path={'/Pages/Contact'} element={<Contact/>} />
+    </Routes>
+    </BrowserRouter>
+    </StudentContextProvider>
+  );
+}
+
+export default App;
